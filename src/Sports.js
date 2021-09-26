7d7fb6d840a5f6abf80caa563635d7e8ff9a90c7
sundharsmart20 <89132033+sundharsmart20@users.noreply.github.com>
2021-09-26 14:20:35 +0530

    onTopChange

--- src/Sports.js
+++ src/Sports.js
@@ -7,6 +7,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import "./App.css"
 
 const Sports=(props)=>{
 
@@ -27,6 +28,33 @@ const Sports=(props)=>{
 			}
 		})
 	}
+	
+		let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+// mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+	
 	return(
 		<div class="position-relative">
 
@@ -75,7 +103,15 @@ const Sports=(props)=>{
 				})
 					
 				}
-			
+			<button
+			        type="button"
+			        class="btn btn-danger btn-floating btn-lg"
+			        id="btn-back-to-top"
+			        onClick={backToTop}
+			        >
+			  &#8593;	
+			</button>	
+				
 		</div>
 		</div>
 		)
