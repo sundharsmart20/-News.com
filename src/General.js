41ca1bb66669592f546725167df32b3b88a796da
sundharsmart20 <89132033+sundharsmart20@users.noreply.github.com>
2021-09-26 14:23:52 +0530

    ontop btn add

--- src/General.js
+++ src/General.js
@@ -30,8 +30,30 @@ const General=(props)=>{
 			}
 		})
 	}
-	console.log("ygjh",props)
+	let mybutton = document.getElementById("btn-back-to-top");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+// mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 	return(
 		<>
 		<div class="shadow p-3 mb-5 bg-body rounded">
@@ -78,6 +100,14 @@ const General=(props)=>{
 				})
 					
 				}
+			<button
+			        type="button"
+			        class="btn btn-danger btn-floating btn-lg"
+			        id="btn-back-to-top"
+			        onClick={backToTop}
+			        >
+			  &#8593;	
+			</button>	
 		</div>
 
 		</>
