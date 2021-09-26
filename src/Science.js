816ca1d17612851869213dc1c5f6f3217044abf3
sundharsmart20 <89132033+sundharsmart20@users.noreply.github.com>
2021-09-26 14:19:15 +0530

    onTop change

--- src/Science.js
+++ src/Science.js
@@ -7,6 +7,8 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import "./App.css"
+
 const Science=(props)=>{
 
 	const[science,setTech]=useState([])
@@ -28,6 +30,33 @@ const Science=(props)=>{
 			}
 		})
 	}
+	
+				let mybutton = document.getElementById("btn-back-to-top");
+
+		// When the user scrolls down 20px from the top of the document, show the button
+		window.onscroll = function () {
+		  scrollFunction();
+		};
+
+		function scrollFunction() {
+		  if (
+		    document.body.scrollTop > 20 ||
+		    document.documentElement.scrollTop > 20
+		  ) {
+		    mybutton.style.display = "block";
+		  } else {
+		    mybutton.style.display = "none";
+		  }
+		}
+		// When the user clicks on the button, scroll to the top of the document
+		// mybutton.addEventListener("click", backToTop);
+
+		function backToTop() {
+		  document.body.scrollTop = 0;
+		  document.documentElement.scrollTop = 0;
+		}
+
+	
 	return(
 		<div class="position-relative">
 
@@ -76,7 +105,15 @@ const Science=(props)=>{
 				})
 					
 				}
-			
+			<button
+			        type="button"
+			        class="btn btn-danger btn-floating btn-lg"
+			        id="btn-back-to-top"
+			        onClick={backToTop}
+			        >
+			  &#8593;	
+			</button>	
+
 		</div>
 		</div>
 		)
